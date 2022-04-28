b86372d5f911be02e7b022604c6d8fb61d0e597a
adarsh3699 <adarsh3699@gmail.com>
2022-04-29 00:40:01 +0530

    home done

--- home1.php
+++ home1.php
@@ -375,45 +375,45 @@ include('OTPfunction.php');
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-            <div class="mobile-footer-logo">
-                <img src="logo.png" alt="Learning Ladder" class="mob-footer-img">
+                <!-- <div class="mobile-footer-logo"> -->
+                    <img src="logo.png" alt="Learning Ladder" class="footer-img">
+                <!-- </div> -->
+                <!-- <div class="contact-det"> -->
+                    <ul class="cont-det">
+                        <li> <i class="fas fa-envelope"></i><a href="mailto:contact@beawiz.com"> contact@beawiz.com</a></li>
+                        <li> <i class="fas fa-phone"></i><a href="#"> +91-8943600513</a></li> 
+                    </ul>
+                <!-- </div> -->
+                <!-- <div class="social-links"> -->
+                        <ul class="social-icons">
+                            <li> <a href="https://www.facebook.com/beawiz" target="_blank"><i style="color: #38539b" class="fab fa-facebook fb"></i></a></li>
+                            <li> <a href="https://twitter.com/beAwiz2017" target="_blank"><i style="color: #58afe0" class="fab fa-twitter twitter"></i></a> </li>
+                        </ul>
+                <!-- </div> -->
             </div>
-            <div class="contact-det">
-                <ul class="cont-det">
-                    <li> <i class="fas fa-envelope"></i><a href="mailto:contact@beawiz.com"> contact@beawiz.com</a></li>
-                    <li> <i class="fas fa-phone"></i><a href="#"> +91-8943600513</a></li> 
-                </ul>
-            </div>
-            <div class="social-links">
-                    <ul class="social-icons">
-                        <li> <a href="https://www.facebook.com/beawiz" target="_blank"><i style="color: #38539b" class="fab fa-facebook fb"></i></a></li>
-                        <li> <a href="https://twitter.com/beAwiz2017" target="_blank"><i style="color: #58afe0" class="fab fa-twitter twitter"></i></a> </li>
+            <div class="col-md-3">
+                <div class="links-sec">
+                    <h4>Know Us</h4>
+                    <ul id="footer-links">
+                        <li><a href="home">Home</a></li>
+                        <li><a href="about-us">About Us</a></li>
+                        <li><a href="its-for-you">Its For You</a></li>
+                        <li><a href="pricing">Pricing</a></li>
+                        <li><a href="learning-portfolio">Learning Portfolio</a></li>
+                        <li><a href="contact">Contact Us</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-md-3">
-            <div class="links-sec">
-                <h4>Know Us</h4>
-                <ul id="footer-links">
-                    <li><a href="home">Home</a></li>
-                    <li><a href="about-us">About Us</a></li>
-                    <li><a href="its-for-you">Its For You</a></li>
-                    <li><a href="pricing">Pricing</a></li>
-                    <li><a href="learning-portfolio">Learning Portfolio</a></li>
-                    <li><a href="contact">Contact Us</a></li>
-                </ul>
-            </div>
-            </div>
-            <div class="col-md-3">
-            <div class="links-sec">
-                    <h4>Legal</h4>
-                <ul id="footer-links">
-                    <li><a href="terms-and-conditions">Terms & Conditions</a></li>
-                    <li><a href="privacy-policy">Privacy Policy</a></li>
-                    <li><a href="refund-policy">Refund Policy</a></li>
-                    <li><a href="faq">FAQs</a></li>
-                </ul>
-            </div>
+                <div class="links-sec">
+                        <h4>Legal</h4>
+                    <ul id="footer-links">
+                        <li><a href="terms-and-conditions">Terms & Conditions</a></li>
+                        <li><a href="privacy-policy">Privacy Policy</a></li>
+                        <li><a href="refund-policy">Refund Policy</a></li>
+                        <li><a href="faq">FAQs</a></li>
+                    </ul>
+                </div>
             </div>
             <div class="col-md-3">
                 <div class="links-sec">
